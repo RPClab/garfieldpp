3089fb16967fc65538b08d0d0f35bb4fd49ef1b0
hschindl <heinrich.schindler@cern.ch>
2019-02-26 08:55:57 +0100

    Fix in Examples/Voxel/savefield

--- Examples/Voxel/savefield.C
+++ Examples/Voxel/savefield.C
@@ -26,30 +26,33 @@ int main(int argc, char * argv[]) {
   si.SetSaturationVelocityModelCanali();
   si.SetHighFieldMobilityModelCanali();
 
-  // Thickness of silicon [cm]
+  // Thickness of silicon [cm].
   constexpr double gap = 50.e-4;
+  // Length in the x and z direction [cm].
+  constexpr double width = 3 * gap;
+
   // Build the geometry.
-  SolidBox box(0., gap / 2., 0., 2., gap / 2., 2.);
+  SolidBox box(0., 0.5 * gap, 0., 0.5 * width, 0.5 * gap, 0.5 * width);
   GeometrySimple geo;
   geo.AddSolid(&box, &si);
 
-  // Make a component with analytic weighting field for a strip or pixel.
+  // Make a component with analytic weighting field for a pixel.
   constexpr double pitch = 50.e-4;
   ComponentAnalyticField wField;
   wField.SetGeometry(&geo);
   wField.AddPlaneY(0., -1., "back");
   wField.AddPlaneY(gap, 0., "front");
-  wField.AddPixelOnPlaneY(0., -0.5 * pitch, 0.5 * pitch, 
-                              -0.5 * pitch, 0.5 * pitch, "pixel");
+  wField.AddPixelOnPlaneY(gap, -0.5 * pitch, 0.5 * pitch, 
+                               -0.5 * pitch, 0.5 * pitch, "pixel");
   wField.AddReadout("pixel");
 
   const double xmin = 0.;
-  const double xmax = 2 * pitch;
-  const unsigned int nX = 100;
+  const double xmax = width;
+  const unsigned int nX = 150;
   const double dx = (xmax - xmin) / nX;
   const double zmin = 0.;
-  const double zmax = 2 * pitch;
-  const unsigned int nZ = 100;
+  const double zmax = width;
+  const unsigned int nZ = 150;
   const double dz = (zmax - zmin) / nZ;
   const double ymin = 0.;
   const double ymax = gap;
@@ -60,12 +63,12 @@ int main(int argc, char * argv[]) {
   std::ofstream outfile;
   outfile.open(filename.c_str(), std::ios::out);
   for (unsigned int i = 0; i < nX; ++i) {
-    const double x = (i + 0.5) * dx;
+    const double x = xmin + (i + 0.5) * dx;
     std::cout << i << " (x = " << x << ")\n";
     for (unsigned int j = 0; j < nY; ++j) {
-      const double y = (j + 0.5) * dy;
+      const double y = ymin + (j + 0.5) * dy;
       for (unsigned int k = 0; k < nZ; ++k) {
-        const double z = (k + 0.5) * dz;
+        const double z = zmin + (k + 0.5) * dz;
         double wx = 0., wy = 0., wz = 0.;
         wField.WeightingField(x, y, z, wx, wy, wz, "pixel");
         double v = wField.WeightingPotential(x, y, z, "pixel");
