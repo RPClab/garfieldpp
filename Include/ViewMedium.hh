02e419227c0573c10428e5ab60531e79ac03a91b
hschindl <heinrich.schindler@cern.ch>
2019-03-17 17:35:43 +0100

    Overhaul ViewMedium.

--- Include/ViewMedium.hh
+++ Include/ViewMedium.hh
@@ -28,40 +28,40 @@ class ViewMedium {
   // Set the medium from which to retrieve the transport coefficients.
   void SetMedium(Medium* m);
 
+  /// Try to choose the x-axis range based on the field grid. 
+  void EnableAutoRangeX(const bool on = true) { m_autoRangeX = on; } 
   /// Set the limits of the electric field.
-  void SetElectricFieldRange(const double emin, const double emax);
+  void SetRangeE(const double emin, const double emax, const bool logscale);
   /// Set the limits of the magnetic field.
-  void SetMagneticFieldRange(const double bmin, const double bmax);
+  void SetRangeB(const double bmin, const double bmax, const bool logscale);
   /// Set the limits of the angle between electric and magnetic field.
-  void SetBAngleRange(const double amin, const double amax);
+  void SetRangeA(const double amin, const double amax, const bool logscale);
+  /// Choose the y-axis range based on the function's minima/maxima.
+  void EnableAutoRangeY(const bool on = true) { m_autoRangeY = on; }
   /// Set the range of the function (velocity etc.) to be plotted.
-  void SetFunctionRange(const double vmin, const double vmax);
-  /// Use the default function range.
-  void SetFunctionRange();
+  void SetRangeY(const double ymin, const double ymax, 
+                 const bool logscale = false);
 
-  void PlotElectronVelocity(const char xaxis, const double e, const double b,
-                            const double a = HalfPi);
-  void PlotHoleVelocity(const char xaxis, const double e, const double b,
-                        const double a = HalfPi);
-  void PlotIonVelocity(const char xaxis, const double e, const double b,
-                       const double a = HalfPi);
-  void PlotElectronDiffusion(const char xaxis, const double e, const double b,
-                             const double a = HalfPi);
-  void PlotHoleDiffusion(const char xaxis, const double e, const double b,
-                         const double a = HalfPi);
-  void PlotIonDiffusion(const char xaxis, const double e, const double b,
-                        const double a = HalfPi);
-  void PlotElectronTownsend(const char xaxis, const double e, const double b,
-                            const double a = HalfPi);
-  void PlotHoleTownsend(const char xaxis, const double e, const double b,
-                        const double a = HalfPi);
-  void PlotElectronAttachment(const char xaxis, const double e, const double b,
-                              const double a = HalfPi);
-  void PlotHoleAttachment(const char xaxis, const double e, const double b,
-                          const double a = HalfPi);
-  void PlotElectronLorentzAngle(const char xaxis, const double e,
-                                const double b, const double a = HalfPi);
+  /// Set the electric field to use when plotting as function of B or angle.
+  void SetElectricField(const double efield) { m_efield = efield; }
+  /// Set the magnetic field to use when plotting as function of E or angle.
+  void SetMagneticField(const double bfield) { m_bfield = bfield; }
+  /// Set the angle to use when plotting as function of E or B.
+  void SetAngle(const double angle) { m_angle = angle; }
+
+  void PlotElectronVelocity(const char xaxis, const bool same = false);
+  void PlotHoleVelocity(const char xaxis, const bool same = false);
+  void PlotIonVelocity(const char xaxis, const bool same = false);
+  void PlotElectronDiffusion(const char xaxis, const bool same = false);
+  void PlotHoleDiffusion(const char xaxis, const bool same = false);
+  void PlotIonDiffusion(const char xaxis, const bool same = false);
+  void PlotElectronTownsend(const char xaxis, const bool same = false);
+  void PlotHoleTownsend(const char xaxis, const bool same = false);
+  void PlotElectronAttachment(const char xaxis, const bool same = false);
+  void PlotHoleAttachment(const char xaxis, const bool same = false);
+  void PlotElectronLorentzAngle(const char xaxis, const bool same = false);
   void PlotElectronCrossSections();
+
   double EvaluateFunction(double* pos, double* par);
 
   enum Property {
@@ -97,41 +97,43 @@ class ViewMedium {
 
   Medium* m_medium = nullptr;
 
-  // Ranges for variable parameters
-  double m_eMin = 0., m_eMax = 1000.;
-  double m_bMin = 0., m_bMax = 5.;
-  double m_aMin = 0., m_aMax = 3.14;
-  double m_vMin = 0., m_vMax = 0.;
+  // X-axis ranges
+  double m_eMin = 100., m_eMax = 100000.;
+  double m_bMin = 0., m_bMax = 2.;
+  double m_aMin = 0., m_aMax = Pi;
+  bool m_logE = true;
+  bool m_logB = false;
+  bool m_logA = false;
+  bool m_autoRangeX = true;
+  // Y-axis range
+  double m_yMin = 0., m_yMax = 1.;
+  bool m_logY = false;
+  bool m_autoRangeY = true;
 
-  // Fixed parameters
-  double m_efield = 500.;
-  double m_bfield = 100.;
-  double m_angle = 0.;
-
-  // Tolerances for marker plotting
-  double m_etolerance = 1.;
-  double m_btolerance = 0.01;
-  double m_atolerance = 0.05;
-
-  // Labels
-  std::string m_labele = "electric field [V/cm]";
-  std::string m_labelb = "magnetic field [T]";
-  std::string m_labela = "magnetic field angle [rad]";
-  std::string m_labelv = "drift velocity [cm/ns]";
-  std::string m_labeld = "diffusion coefficient [#sqrt{cm}]";
+  // E-field to use when plotting as function of B-field or angle.
+  double m_efield = 1000.;
+  // B-field to use when plotting as function of E-field or angle.
+  double m_bfield = 0.;
+  // Angle to use when plotting as function of E-field or B-field.
+  double m_angle = HalfPi;
 
   // Functions
   std::vector<TF1> m_functions;
   // Graphs
-  std::vector<TGraph> m_graphs;
+  std::vector<TGraph*> m_graphs;
+  // Labels
+  std::vector<std::pair<std::string, int> > m_labels;
 
   void SetupCanvas();
-  void AddFunction(const double xmin, const double xmax, const double ymin,
-                   const double ymax, const bool keep,
-                   const std::string& xlabel, const std::string& ylabel,
-                   const int type, const char xaxis, const double e,
-                   const double b, const double a);
+  void AddFunction(const bool keep, const Property type, const char xaxis);
+  void GetAxisRangeX(const std::vector<double>& efields,
+                     const std::vector<double>& bfields, 
+                     const std::vector<double>& angles, const char xaxis,
+                     double& xmin, double& max, bool& logx) const;
   int GetColor(const Property property) const;
+  std::string GetLabelX(const char xaxis) const;
+  std::string GetLabelY(const Property property) const;
+  std::string GetLegend(const Property property) const;
 };
 }
 #endif
